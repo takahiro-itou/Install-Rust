c46d02efa27bd9f2f6c54c147f2e47ec09583fce
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-02-16 19:53:18 +0900

    ディレクトリをバックアップ復元する機能を実装

---
+++ install-rust.sh
@@ -0,0 +1,24 @@
+#!/bin/bash  -xue
+
+script_dir=$(dirname "$0")
+cur_time=$(date +%Y%m%d-%H%M%S)
+
+function escape_directory() {
+    local _dir=$1
+    local _sfx=${2:-"${cur_time}"}
+
+    if [[ -d "${_dir}" ]] ; then
+        mv -v "${_dir}" "${_dir}.${sfx}"
+    fi
+    return 0
+}
+
+function restore_directory() {
+    local _dir=$1
+    local _sfx=${2:-"${cur_time}"}
+
+    if [[ -d "${_dir}.${sfx}" && ! -d "${_dir}" ]] ; then
+        mv -v "${_dir}.${sfx}" "${_dir}"
+    fi
+    return 0
+fi
