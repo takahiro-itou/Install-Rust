14af2591518032622e98c1bbf011f2297d95eb54
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-02-16 19:57:02 +0900

    不具合修正

--- install-rust.sh
+++ install-rust.sh
@@ -8,7 +8,7 @@ function escape_directory() {
     local _sfx=${2:-"${cur_time}"}
 
     if [[ -d "${_dir}" ]] ; then
-        mv -v "${_dir}" "${_dir}.${sfx}"
+        mv -v "${_dir}" "${_dir}.${_sfx}"
     fi
     return 0
 }
@@ -17,8 +17,8 @@ function restore_directory() {
     local _dir=$1
     local _sfx=${2:-"${cur_time}"}
 
-    if [[ -d "${_dir}.${sfx}" && ! -d "${_dir}" ]] ; then
-        mv -v "${_dir}.${sfx}" "${_dir}"
+    if [[ -d "${_dir}.${_sfx}" && ! -d "${_dir}" ]] ; then
+        mv -v "${_dir}.${_sfx}" "${_dir}"
     fi
     return 0
 }
